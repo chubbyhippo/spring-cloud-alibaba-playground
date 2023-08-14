99d0b7a390f8096ef1e3d0a900c1793b709d2026
ch <mkmsacc@outlook.com>
2023-08-14 17:48:27 +0700

    Remove nacos config

--- nacos/nacos-server-docker/init/nacos-config-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Nacos auto config started"
-exampleConfig=$(cat ../config/nacos-config-example.properties)
-groupId="GROUP"
-curl -X POST "http://127.0.0.1:8848/nacos/v1/cs/configs" -d "dataId=datasource-config.yaml&group=${groupId}&content=${exampleConfig}"
-echo "Nacos config pushed successfully finished"
