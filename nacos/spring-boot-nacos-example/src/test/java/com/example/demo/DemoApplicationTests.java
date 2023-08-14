9cfe98da282c7df412c91d0c930b5f40cd5bd8a8
ch <mkmsacc@outlook.com>
2023-08-14 13:05:09 +0700

    Add tests

--- nacos/spring-boot-nacos-example/src/test/java/com/example/demo/DemoApplicationTests.java
+++ nacos/spring-boot-nacos-example/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.interfaces.rest.dto.NacosConfigResource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+	@Autowired
+	private WebTestClient client;
+	@Test
+	void shouldRun() {
+		assertThatNoException().isThrownBy(() -> DemoApplication.main(new String[]{}));
+
+	}
 
 	@Test
-	void contextLoads() {
+	void shouldGetNacosConfig() {
+		client.get()
+				.uri("")
+				.exchange()
+				.expectBody(NacosConfigResource.class);
 	}
 
 }
