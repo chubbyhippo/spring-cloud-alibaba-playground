d0c22eaffb7803e8d7d18d80f1bb4b16cfd206b9
ch <mkmsacc@outlook.com>
2023-08-14 13:05:02 +0700

    Refactor controller

--- nacos/spring-boot-nacos-example/src/main/java/com/example/demo/interfaces/rest/NacosController.java
+++ nacos/spring-boot-nacos-example/src/main/java/com/example/demo/interfaces/rest/NacosController.java
@@ -1,28 +1,26 @@
 package com.example.demo.interfaces.rest;
 
 import com.example.demo.domain.NacosConfig;
+import com.example.demo.interfaces.rest.dto.NacosConfigResource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/nacos/bean")
 @RequiredArgsConstructor
 public class NacosController {
     private final NacosConfig nacosConfig;
+    private final NacosConfigDtoConverter nacosConfigDtoConverter;
 
     @GetMapping
-    Map<String, String> getConfig(){
-        var result = new HashMap<String, String>();
-        result.put("serverAddr", nacosConfig.getServerAddr());
-        result.put("prefix", nacosConfig.getPrefix());
-        result.put("group", nacosConfig.getGroup());
-        result.put("namespace", nacosConfig.getNamespace());
-
-        return result;
+    NacosConfigResource getConfig() {
+        return nacosConfigDtoConverter.toNacosConfigResource(NacosConfig.builder()
+                .serverAddr(nacosConfig.getServerAddr())
+                .prefix(nacosConfig.getPrefix())
+                .group(nacosConfig.getGroup())
+                .namespace(nacosConfig.getNamespace())
+                .build());
     }
 }
