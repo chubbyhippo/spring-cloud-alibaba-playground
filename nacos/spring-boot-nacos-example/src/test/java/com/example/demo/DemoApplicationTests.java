04e70c95551c97792fca77fdbce0722e193be3b2
ch <mkmsacc@outlook.com>
2023-08-14 13:39:18 +0700

    Refactor test

--- nacos/spring-boot-nacos-example/src/test/java/com/example/demo/DemoApplicationTests.java
+++ nacos/spring-boot-nacos-example/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,24 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
-	@Autowired
-	private WebTestClient client;
-	@Test
-	void shouldRun() {
-		assertThatNoException().isThrownBy(() -> DemoApplication.main(new String[]{}));
+    @Autowired
+    private WebTestClient client;
 
-	}
+    @Test
+    void shouldRun() {
+        assertThatNoException().isThrownBy(() -> DemoApplication.main(new String[]{}));
 
-	@Test
-	void shouldGetNacosConfig() {
-		client.get()
-				.uri("")
-				.exchange()
-				.expectBody(NacosConfigResource.class);
-	}
+    }
+
+    @Test
+    void shouldGetNacosConfig() {
+        var expected = NacosConfigResource.builder()
+                .serverAddr("127.0.0.1:8848")
+                .prefix("PREFIX")
+                .group("GROUP")
+                .namespace("NAMESPACE")
+                .build();
+
+        var nacosConfigResource = client.get()
+                .uri("/nacos/bean")
+                .exchange()
+                .expectBody(NacosConfigResource.class)
+                .returnResult()
+                .getResponseBody();
+
+        assertThat(nacosConfigResource).isEqualTo(expected);
+
+    }
 
 }
