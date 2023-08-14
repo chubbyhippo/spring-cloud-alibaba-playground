0bf1eb158885c492dbc87a7845614b6aec3c5a40
ch <mkmsacc@outlook.com>
2023-08-14 13:04:47 +0700

    Add builder for nacos config

--- nacos/spring-boot-nacos-example/src/main/java/com/example/demo/domain/NacosConfig.java
+++ nacos/spring-boot-nacos-example/src/main/java/com/example/demo/domain/NacosConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain;
 
 
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -8,6 +9,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 @ConfigurationProperties(prefix = "spring.cloud.nacos.config")
 @RefreshScope
 @Data
+@Builder
 public class NacosConfig {
 
     String serverAddr;
